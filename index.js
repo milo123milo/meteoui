841ebe0eb27866dc422c01f915d413004f30609d
milo123milo <kosmonauti220@gmail.com>
2023-08-11 23:38:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 3300;
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next(); // Call the next middleware/route handler
+});
+
 // Define a route to render the EJS template
 app.get('/', (req, res) => {
   res.render('index');
